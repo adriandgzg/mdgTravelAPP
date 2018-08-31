9df5e9cf42d503e5c8043502641a84c809f04c2a
Adrian DOminguez <adominguez@grupoasesores.com.mx>
2018-08-30 20:44:20 -0500

    elimine una linea que escribe un titulo en el VC

--- pruebavisuales/ViewController.swift
+++ pruebavisuales/ViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationController?.title = "TravelApp"
+    
         self.title = "TravelAPP"
     }
 
